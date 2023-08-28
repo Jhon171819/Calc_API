8423fb2d871f8215bc28563e25ae862f8cb17ca7
Jhon171819 <jonathancosta1717@gmail.com>
2023-08-28 15:26:23 -0300

    commit teste

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const cors = require("cors")
 const port = 3000;
 
-app.use(cors())
+app.use(cors({
+    origin: ['https://calc-api.vercel.app/', 'https://www.google.com/', "https://jhon171819.github.io/Calc_API/"]
+}))
 app.use(express.json());
 
 app.use(express.static('https://jhon171819.github.io/Calc_API/' + 'Client'));
