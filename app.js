7a6b9431b630fb6f77932563ad9b00f410615861
Jhon171819 <141761671+Jhon171819@users.noreply.github.com>
2023-08-28 15:32:24 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require("cors")
 const port = 3000;
+const whitelist = ['https://calc-api.vercel.app/', 'https://www.google.com/', "https://jhon171819.github.io/Calc_API/"]
+
 
 app.use(cors({
     origin: ['https://calc-api.vercel.app', 'https://www.google.com/', "https://jhon171819.github.io/Calc_API/"]
