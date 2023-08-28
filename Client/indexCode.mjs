c31112f21bb37c02cc07d83dd361bcdacee0a136
Jhon171819 <jonathancosta1717@gmail.com>
2023-08-28 16:34:36 -0300

    update commit

--- Client/indexCode.mjs
+++ Client/indexCode.mjs
@@ -1,9 +1,8 @@
 async function performOperation(operation) {
     const valor1 = parseFloat(document.getElementById("valor1").value);
     const valor2 = parseFloat(document.getElementById("valor2").value);
-
-
-    const response = await fetch(`https://jhon171819.github.io/Calc_API/${operation}`, {
+    const url = `https://jhon171819.github.io/Calc_API/${operation}`
+    const opcoes =  {
         method: 'POST', 
         mode: 'no-cors',
 
@@ -11,10 +10,15 @@ async function performOperation(operation) {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({valor1, valor2})
-    });
-    const data = await response.json();
-    document.getElementById("resultado").textContent = `${data.result}`;
-}
+    };
+
+    fetch(url,opcoes)
+        .then(const data = await response.json();)
+        .then(document.getElementById("resultado").textContent = `${data.result}`;
+
+
+    
+    
 
 function modoClaro() {
     document.getElementById("corpo").style.backgroundColor = "white";
