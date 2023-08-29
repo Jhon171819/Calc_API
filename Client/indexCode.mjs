251dccc5a756da7ce77ade33d2abd40fccdc545e
Jhon171819 <jonathancosta1717@gmail.com>
2023-08-29 15:44:17 -0300

    update link

--- Client/indexCode.mjs
+++ Client/indexCode.mjs
@@ -1,23 +1,27 @@
 async function performOperation(operation) {
     const valor1 = parseFloat(document.getElementById("valor1").value);
     const valor2 = parseFloat(document.getElementById("valor2").value);
-    const url = `https://jhon171819.github.io/Calc_API/${operation}`
-    const opcoes =  {
+    const url = `http://localhost:3000/${operation}`;
+    const data = {
+        valor1: valor1,
+        valor2: valor2
+    };
+    const opcoes = {
         method: 'POST', 
-        mode: 'no-cors',
-
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({valor1, valor2})
+        body: JSON.stringify(data) 
     };
 
-    const response = fetch(url,opcoes)
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById("resultado").textContent = `${data.result}`;
-        })
-        }
+    try {
+        const response = await fetch(url, opcoes);
+        const responseData = await response.json();
+        document.getElementById("resultado").textContent = `${responseData.resultado}`;
+    } catch (error) {
+        console.error("Ocorreu um erro:", error);
+    }
+}
     
     
 
