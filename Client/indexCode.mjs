d1a77647809b02a929b07507ce22aec0df6fd237
Jhon171819 <jonathancosta1717@gmail.com>
2023-09-01 16:49:26 -0300

    update

--- Client/indexCode.mjs
+++ Client/indexCode.mjs
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const opcoes = {
         method: 'POST',
         headers: {
-            Content-Type: 'application/json'
+            'Content-Type' : 'application/json'
         },
         body: JSON.stringify({operation: 'adi'})
     }
