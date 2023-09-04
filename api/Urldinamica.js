6f7196b1e8b659c1e22d5bc8a2ec27e53d26895a
Jhon171819 <jonathancosta1717@gmail.com>
2023-09-04 16:33:52 -0300

    updateServerless

--- api/Urldinamica.js
+++ api/Urldinamica.js
@@ -7,32 +7,48 @@ const actUrl = "/api";
 urldinamica.use(express.static("https://calc-api.vercel.app/" + "Client"));
 
 urldinamica.get('/api/Urldinamica', (req,res) => res.send("get funcionando"))
-exports.handler = async function (event) {
-    const eventBody = JSON.parse(event.body).operation;   
-    if (eventBody == "adi") {
-        console.log('entramos na SF')
-    try {
-        const { valor1, valor2 } = JSON.parse(event.body);
-        const result = valor1 + valor2;
+// exports.handler = async function (event) {
+//     const eventBody = JSON.parse(event.body).operation;   
+//     if (eventBody == "adi") {
+//         console.log('entramos na SF')
+//     try {
+//         const { valor1, valor2 } = JSON.parse(event.body);
+//         const result = valor1 + valor2;
         
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ result })
-    };
-    } catch (error) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({ error: 'Erro na requisição' })
-        };
-    } }
-    }
+//     return {
+//         statusCode: 200,
+//         body: JSON.stringify({ result })
+//     };
+//     } catch (error) {
+//         return {
+//             statusCode: 400,
+//             body: JSON.stringify({ error: 'Erro na requisição' })
+//         };
+//     } }
+//     }
     
+module.exports = (req, res) => {
+    if (req.method === 'POST') {
+      // Lógica de manipulação de solicitações POST aqui
+      const requestBody = req.body;
+      
+      // Realize alguma operação com os dados do corpo da solicitação
+      const resultado = requestBody.valor1 + requestBody.valor2;
+  
+      res.status(200).json({ resultado });
+    } else {
+      res.status(405).json({ error: 'Método não permitido' });
+    }
+  };
 
 
 urldinamica.post(actUrl + '/adi', (req, res) => {
-    const { valor1, valor2 } = req.body;
-    const result = valor1 + valor2;
-    res.json({ result });
+    const requestBody = req.body;
+  
+  // Realize alguma operação com os dados do corpo da solicitação
+  const resultado = requestBody.valor1 + requestBody.valor2;
+
+  res.status(200).json({ resultado });
 });
 urldinamica.post(actUrl + '/mult', (req, res) => {
     const { valor1, valor2 } = req.body;
