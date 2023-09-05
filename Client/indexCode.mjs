0d625edb125c956dd6e2bea027134d25b63fbaf0
Jhon171819 <jonathancosta1717@gmail.com>
2023-09-05 15:16:15 -0300

    update

--- Client/indexCode.mjs
+++ Client/indexCode.mjs
@@ -3,60 +3,89 @@ document.addEventListener('DOMContentLoaded', () => {
     const multB = document.getElementById("mult");
     const subB = document.getElementById("sub");
     const divB = document.getElementById("div")
-    function pegarValores() {
-        const valor1 = parseFloat(document.getElementById("valor1").value);
-        const valor2 = parseFloat(document.getElementById("valor2").value);
-        return {valor1, valor2}
-    }
-    
-    const opcoes = {
-        
-        method: 'POST',
-        headers: {
-            'Content-Type' : 'application/json'
-        },
-        body: JSON.stringify(pegarValores().valor1, pegarValores().valor2)
-    }
     
     somarB.addEventListener('click', async () =>{
-        console.log(pegarValores(), pegarValores().valor1)
-        console.log(opcoes)
-        console.log('o botão esta indo')
+        const valores = pegarValores(); // Obtenha os valores dos campos aqui
+        const opcoes = {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(valores) // Use os valores obtidos aqui
+        };
+
+        console.log(valores);
+        console.log(opcoes);
+        
         const response = await fetch('http://localhost:3000/adi', opcoes)
-        responseData = await response.json();
+        const responseData = await response.json();
         console.log('passamos pelo fetch')
         document.getElementById('resultado').textContent = `${responseData.resultado}`;
-
-    })
+    });
 
     multB.addEventListener('click', async () =>{
-        console.log('o botão esta indo')
+        const valores = pegarValores();
+        const opcoes = {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(valores)
+        };
+
+        console.log(valores);
+        console.log(opcoes);
+        
         const response = await fetch('http://localhost:3000/mult', opcoes)
-        responseData = await response.json();
+        const responseData = await response.json();
         console.log('passamos pelo fetch')
         document.getElementById('resultado').textContent = `${responseData.resultado}`;
-
-    })
+    });
 
     subB.addEventListener('click', async () =>{
-        console.log('o botão esta indo')
+        const valores = pegarValores();
+        const opcoes = {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(valores)
+        };
+
+        console.log(valores);
+        console.log(opcoes);
+        
         const response = await fetch('http://localhost:3000/sub', opcoes)
-        responseData = await response.json();
+        const responseData = await response.json();
         console.log('passamos pelo fetch')
         document.getElementById('resultado').textContent = `${responseData.resultado}`;
-
-    })
+    });
 
     divB.addEventListener('click', async () =>{
-        console.log('o botão esta indo')
+        const valores = pegarValores();
+        const opcoes = {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(valores)
+        };
+
+        console.log(valores);
+        console.log(opcoes);
+        
         const response = await fetch('http://localhost:3000/divs', opcoes)
-        responseData = await response.json();
+        const responseData = await response.json();
         console.log('passamos pelo fetch')
         document.getElementById('resultado').textContent = `${responseData.resultado}`;
-        
-    })
+    });
 })
-    
+
+function pegarValores() {
+    const valor1 = parseFloat(document.getElementById("valor1").value);
+    const valor2 = parseFloat(document.getElementById("valor2").value);
+    return { valor1, valor2 };
+}
 
 function modoClaro() {
     document.getElementById("corpo").style.backgroundColor = "white";
